fb36f1de39988d7012b8004286aa1844c5fc4433
Sepehr Hajbarat <sepehrhaj77@gmail.com>
2021-02-01 19:01:07 -0500

    remove dotenv code

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express()
-require('dotenv').config()
 const mongoose = require('mongoose');
 const path = require('path');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
+//require('dotenv').config()
 
 const port = process.env.PORT || 3000;
 
